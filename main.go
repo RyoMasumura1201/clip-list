802d6766d24bb7c6e04deccae0020035eb9e4fee
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-07-18 00:37:48 +0900

    初期ファイルを作成するように

--- main.go
+++ main.go
@@ -4,8 +4,24 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/RyoMasumura1201/clip-list/cmd"
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/RyoMasumura1201/clip-list/cmd"
+)
 
 func main() {
+	curDir, _ := os.Getwd()
+	filePath := filepath.Join(curDir, ".clipList")
+
+	file, err := os.OpenFile(filePath, os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
 	cmd.Execute()
 }
